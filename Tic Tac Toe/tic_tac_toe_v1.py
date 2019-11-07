cb829b035e93e6b60442b47a871219ebc99dc3de
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2019-11-07 17:14:42 +0200

    Updating the file
    Updating a few new features , like extended ending with restart and adding more comments for better readability

--- Tic Tac Toe/tic_tac_toe_v1.py
+++ Tic Tac Toe/tic_tac_toe_v1.py
@@ -1,14 +1,16 @@
+# ------------Global Variables----------------
 tic_tac_toe = [
     ["| |", "| |", "| |"],
     ["| |", "| |", "| |"],
     ["| |", "| |", "| |"],
 ]
 
-current_move = 0
-player = ""
-current_position = ""
+current_move = 0  # Counter of the total CORRECT MOVES
+player = ""  # Current player "X or O"
+current_position = ""  # The position selected on the board "A1, B2 or other..."
 
 
+# Function to print the chest
 def print_chess():
     global player
     next_player = 2
@@ -31,6 +33,7 @@ def print_chess():
     print(f"It's time for Player-{next_player} to place their sign: '{next_sign}'.")
 
 
+# The start game function, also used for help menu
 def start():
     print(90*"-"+"\n"+90*"-")
     print("Hello :) You are about to play a game of Tic Tac Toe")
@@ -46,13 +49,16 @@ def start():
     print(f"3 | | | | | |   --> YOU WILL GET  -->   3 | | | | | |")
     print(54 * "-")
     print("OR ENTER 'game' to check your game, 'help' to see this menu again or 'end' to exit.")
+    print("If you feel like RESTARTING, just Type down 'RESTART'. ")
     get_input()
 
 
+# Function to clean the screen... it adds 25 new empty rows.
 def clean_screen():
     print(25*"\n")
 
 
+# Function to get and sort the input. It will set the next player to X or O depending on current valid move.
 def get_input():
     global current_position, player
     if current_move % 2 == 0:
@@ -62,17 +68,21 @@ def get_input():
     sort_input(input("Enter Position:  "), player)
 
 
+# In this function we sort all the inputs from the user.
 def sort_input(text, current_player):
     global tic_tac_toe, current_move
     text = text.upper()
     if text == "HELP":
         start()
     elif text == "END":
+        print((90 * "-") + "\n" + (90 * "-"))
         print("It was a nice game but you lost before even finishing... lame as F***!")
-        exit(0)
+        end_game()
     elif text == "GAME":
         print_chess()
         get_input()
+    elif text == "RESTART":
+        restart()
     else:
         valid_inputs = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3",
                         "1A", "2A", "3A", "1B", "2B", "3B", "1C", "2C", "3C"]
@@ -89,7 +99,7 @@ def sort_input(text, current_player):
                 column = 2
             if tic_tac_toe[row][column] == "| |":
                 tic_tac_toe[row][column] = f"|{current_player}|"
-                check_for_win()
+                check_for_win_or_tie()
                 current_move += 1
                 print_chess()
                 get_input()
@@ -101,13 +111,16 @@ def sort_input(text, current_player):
             get_input()
 
 
-def check_for_win():
+# Just as the name suggest this function will check if the game has ended with win or tie.
+def check_for_win_or_tie():
     winner = None
+
     # Check the horizontal lines:
     for horizontal_line in tic_tac_toe:
         if "| |" not in horizontal_line:
             if len(set(horizontal_line)) == 1:
                 winner = horizontal_line[0]
+
     # check the vertical lines:
     if (tic_tac_toe[0][0] == tic_tac_toe[1][0] == tic_tac_toe[2][0]) and tic_tac_toe[0][0] != "| |":
         winner = tic_tac_toe[0][0]
@@ -115,19 +128,24 @@ def check_for_win():
         winner = tic_tac_toe[0][1]
     elif (tic_tac_toe[0][2] == tic_tac_toe[1][2] == tic_tac_toe[2][2]) and tic_tac_toe[0][2] != "| |":
         winner = tic_tac_toe[0][2]
+
     # Check the diagonal lines:
     elif (tic_tac_toe[0][0] == tic_tac_toe[1][1] == tic_tac_toe[2][2]) and tic_tac_toe[0][0] != "| |":
         winner = tic_tac_toe[0][0]
     elif (tic_tac_toe[0][2] == tic_tac_toe[1][1] == tic_tac_toe[2][0]) and tic_tac_toe[0][2] != "| |":
         winner = tic_tac_toe[0][2]
-    # Check for tie
+
+    # Check for tie and if we have one, print the stage as a TIE.
     if ("| |" not in tic_tac_toe[0]) and ("| |" not in tic_tac_toe[1]) and ("| |" not in tic_tac_toe[2]):
-        tie()
+        print_tie()
+
+    # Check if we have an winner and if so print the winning stage
     if winner:
-        win(winner)
+        print_win(winner)
 
 
-def win(x_or_o):
+# This function is called when there is a win and it will print the winning stage.
+def print_win(x_or_o):
     clean_screen()
     winner_num = 0
     if x_or_o == "|X|":
@@ -142,15 +160,15 @@ def win(x_or_o):
         print(line_number, end=" ")
         print(*line)
         line_number += 1
-
     print(90 * "*"+"\n"+90 * "*")
     print(90 * "*"+"\n"+f"THE WINNER SI PLAYER - {winner_num}"+"\n"+90 * "*")
     print(90 * "*" + "\n" + 90 * "*")
     print("\n\n\nCreated by Simeon Tsvetanov :)")
-    exit(0)
+    end_game()
 
 
-def tie():
+# This function is called when there is a tie and it will print the stage for Tie.
+def print_tie():
     clean_screen()
     print(90 * "-" + "\n" + 90 * "-")
     print("Unfortunately, You are equally big suckers")
@@ -164,7 +182,35 @@ def tie():
     print(f"And its a ----TIE----")
     print("\n\n\nCreated by Simeon Tsvetanov :)")
     print(90 * "-" + "\n" + 90 * "-")
-    exit(0)
+    end_game()
+
+
+# This function if for ending the game.
+def end_game():
+    print("If you want to start over, type down 'RESTART' and hit enter")
+    print("Or If you want to End the game type down end 'END' hit enter")
+    decision = input().upper()
+    if decision == "END":
+        exit(0)
+    elif decision == "RESTART":
+        restart()
+    else:
+        print("\n!!! Invalid Choice !!!")
+        print("Try Again:")
+        end_game()
+
+
+# This function will restart the game
+def restart():
+    clean_screen()
+    global tic_tac_toe, current_move, player, current_position
+    for row in range(len(tic_tac_toe)):
+        for item in range(len(tic_tac_toe[row])):
+            tic_tac_toe[row][item] = "| |"
+    current_move = 0
+    player = ""
+    current_position = ""
+    start()
 
 
 if __name__ == '__main__':
